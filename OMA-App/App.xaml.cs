e9aa14d6673aa1858f1c9dca3d907588d3554a32
Frederik Cornelius Skovgaard <frederik.skovgaard@danfoss.com>
2024-11-05 09:27:54 +0100

    Add project files.

---
+++ OMA-App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OMA_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
