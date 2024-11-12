34a752367329ff949a4fd9395c47daf0b5c36799
rene069 <rene_rasmussen@live.dk>
2024-11-12 11:44:16 +0100

    t

--- OMA-App/Storage/TokenService.cs
+++ OMA-App/Storage/TokenService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,17 @@ namespace OMA_App.Storage
     public static class TokenService
     {
         private const string AccessTokenKey = "access_token";
+        private const string UsernameKey = "username";
 
         public static async Task SaveAccessTokenAsync(string token)
         {
+            var handler = new JwtSecurityTokenHandler();
+
+            var test = handler.ReadJwtToken(token);
+            var s = test.Claims.FirstOrDefault(c => c.Type == "sub").Value;
+
             await SecureStorage.SetAsync(AccessTokenKey, token);
+            await SecureStorage.SetAsync(UsernameKey,  s);
         }
 
         public static async Task<string> GetAccessTokenAsync()
